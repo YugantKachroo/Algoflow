e0df07d21f0dff454b3fc2ea914f7400c9506873
Yugant Kachroo <yugant35@gmail.com>
2020-06-27 19:35:22 +0530

    linear search completed

--- react_front/src/algorithms/Searching/LinearSearch/LinearSearch.js
+++ react_front/src/algorithms/Searching/LinearSearch/LinearSearch.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import './LinearTiles.css';
 import LinearTiles from './LinearTiles';
 import './LinearSearch.css';
-import { Button, Input, Segment, Divider } from 'semantic-ui-react';
 import { RandomInt } from '../../../components/RandomInt';
 import Header from '../../../components/Header';
 
 const ARRAY_BARS = 20;
-const BASE_COLOR = '#63eaf1';
-const FOUND_COLOR = '#6eb428';
-const NOT_FOUND_COLOR = '#f16363';
+const BASE_COLOR = 'purple';
+const FOUND_COLOR = '#32CD32';
+const NOT_FOUND_COLOR = '#6384f1';
 const ANIMATION_SPEED_SECONDS = 0.4;
 
 class LinearSearch extends Component {
   constructor(props) {
     super(props);
-    this.setState = {
+    this.state = {
       array: [],
       found: false,
       message: '',
@@ -24,6 +22,20 @@ class LinearSearch extends Component {
       target: null,
     };
   }
+  linearSearchAnimations = (array, target) => {
+    let animations = [];
+    for (let i = 0; i < array.length; i++) {
+      const ele = array[i];
+      const tar = parseInt(target);
+      if (ele === tar) {
+        animations.push([i, ele, true]);
+        break;
+      } else {
+        animations.push([i, ele, false]);
+      }
+    }
+    return animations;
+  };
 
   componentDidMount() {
     this.Arrayreset();
@@ -47,8 +59,58 @@ class LinearSearch extends Component {
     this.setState({ array, found, disabled, message });
   };
 
+  LinearSearch = async () => {
+    var msg = '';
+    const target = document.getElementById('target').value;
+    if (target === '') {
+      return undefined;
+    }
+    const animations = this.linearSearchAnimations(this.state.array, target);
+    console.log(animations);
+    let count = 0;
+
+    for (let i = 0; i < animations.length; i++) {
+      const [index, currentElement, found] = animations[i];
+      const arraybars = document.getElementsByClassName('linear-array-bar');
+      const arraybar = arraybars[index];
+      const arraybarStyle = arraybar.style;
+      count++;
+      if (found) {
+        msg = `${currentElement} found at index ${index}`;
+        setTimeout(() => {
+          this.setState({
+            disabled: true,
+            found: true,
+            findex: index,
+            target: currentElement,
+          });
+
+          arraybarStyle.backgroundColor = FOUND_COLOR;
+          arraybar.classList.add('growFind');
+          arraybar.classList.add('highlight');
+        }, i * ANIMATION_SPEED_SECONDS * 1000);
+      } else {
+        msg = `${target} not found`;
+        setTimeout(() => {
+          this.setState({
+            disabled: true,
+            found: false,
+          });
+          arraybarStyle.backgroundColor = NOT_FOUND_COLOR;
+          arraybar.classList.add('growFind');
+        }, i * ANIMATION_SPEED_SECONDS * 1000);
+      }
+      await setTimeout(() => {
+        this.setState({
+          disabled: false,
+          message: msg,
+        });
+      }, count * ANIMATION_SPEED_SECONDS * 500);
+    }
+  };
+
   render() {
-    //const { message, disabled, array, found, findex } = this.state;
+    const { message, disabled, array, found } = this.state;
     return (
       <div className='jumbotron jumbotron-fluid bg-light'>
         <center>
@@ -67,11 +129,11 @@ class LinearSearch extends Component {
 
                 <div className='input-group-append'>
                   <button
-                    onClick={() => this.linearSearch()}
-                    className='ui inverted yellow button'
+                    onClick={() => this.LinearSearch()}
+                    className='ui inverted brown button'
                     type='button'
                     id='button-addon2'
-                    //  disabled={disabled}
+                    disabled={disabled}
                   >
                     Search
                   </button>
@@ -80,7 +142,7 @@ class LinearSearch extends Component {
                     className='ui inverted red button'
                     id='resetArray'
                     type='button'
-                    //  disabled={disabled}
+                    disabled={found ? '' : disabled}
                   >
                     Reset Array
                   </button>
@@ -88,6 +150,18 @@ class LinearSearch extends Component {
               </div>
               <div className='col-sm-1 '></div>
             </div>
+          </div>
+          <br />
+          {found ? <p className='found growFind'>{message}</p> : null}
+          <div className='container'>
+            {array.map((value, index) => (
+              <LinearTiles
+                type={`linearSearch`}
+                key={index}
+                index={index}
+                value={value}
+              />
+            ))}
           </div>
         </center>
       </div>
