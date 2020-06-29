28818dc4041f845c502b23c258c600d101ddb37c
Yugant Kachroo <yugant35@gmail.com>
2020-06-29 23:47:09 +0530

    navbar styling improved

--- react_front/src/Menu.js
+++ react_front/src/Menu.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const isActive = (history, path) => {
-  if (history.location.pathname === path) return { color: 'black' };
-  else return { color: 'white' };
+  if (history.location.pathname === path)
+    return { color: 'black', backgroundColor: 'white' };
+  else return { color: 'black' };
 };
 
 const Menu = ({ history }) => (
   <div>
-    <ul className='nav nav-tabs bg-danger'>
+    <ul className='nav nav-tabs bg-info'>
       <li className='nav-item'>
         <Link className='nav-link' style={isActive(history, '/')} to='/'>
           Backtracking
