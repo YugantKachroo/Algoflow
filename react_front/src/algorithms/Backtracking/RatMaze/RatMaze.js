e4f9abf061b78095bcb9e1cf8199c8c226fc0651
Yugant Kachroo <yugant35@gmail.com>
2020-07-18 15:56:14 +0530

    rat maze generated

--- react_front/src/algorithms/Backtracking/RatMaze/RatMaze.js
+++ react_front/src/algorithms/Backtracking/RatMaze/RatMaze.js
@@ -10,11 +10,46 @@ const SOL = MakeBoard(Maze.length);
 const NO_PATH_COLOR = '#ededed';
 const PATH_COLOR = '#ADD2FE';
 const SAFE_COLOR = '#B0FCEF';
+const ANIMATION_SPEED_MS = 1000;
 
 export default class RatMaze extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+    this.drawBoard(Maze);
+  }
+
+  async drawBoard(Maze = []) {
+    const Rows = Maze.length;
+    const Cols = Maze[0].length;
+    const ratMaze = document.getElementById('ratMazeBoard');
+    ratMaze.innerHTML = '';
+    ratMaze.style.setProperty('--Rgrid-rows', Rows);
+    ratMaze.style.setProperty('--Rgrid-cols', Cols);
+    for (let i = 0; i < Rows; i++) {
+      for (let j = 0; j < Cols; j++) {
+        let cell = document.createElement('div');
+        let item = document.createElement('i');
+        let cellStyle = cell.style;
+        cell.appendChild(item).className = 'fas rat';
+        if (i === 0 && j === 0) {
+          cell.appendChild(item).classList.add('flipH');
+          item.innerHTML = '🐁';
+        }
+        if (i === Rows - 1 && j === Cols - 1) {
+          item.innerHTML = '🧀';
+        }
+        if (Maze[i][j] === 1) {
+          cellStyle.backgroundColor = PATH_COLOR;
+        } else {
+          cellStyle.backgroundColor = NO_PATH_COLOR;
+        }
+        ratMaze.appendChild(cell).className = 'Rgrid-item r-array-tile';
+      }
+    }
   }
 
   render() {
