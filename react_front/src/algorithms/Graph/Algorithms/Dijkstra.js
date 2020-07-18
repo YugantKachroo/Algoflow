694cdbf2ffab05e32751947e95cf80c525880bb6
Yugant Kachroo <yugant35@gmail.com>
2020-07-19 03:52:08 +0530

    dijkstra logic added

---
+++ react_front/src/algorithms/Graph/Algorithms/Dijkstra.js
@@ -0,0 +1,72 @@
+export function Dijkstra(grid, startNode, finishNode) {
+  const visitedOrder = [];
+  startNode.distance = 0;
+  const unvisitedNodes = getAllNodes(grid);
+  while (!!unvisitedNodes.length) {
+    sortNodes(unvisitedNodes);
+    const closestNode = unvisitedNodes.shift();
+    if (closestNode.isWall) {
+      continue;
+    }
+    if (closestNode.distance === Infinity) {
+      return [visitedOrder, calculatePath(finishNode)];
+    }
+    closestNode.isVisited = true;
+    visitedOrder.push(closestNode);
+
+    if (closestNode === finishNode) {
+      return [visitedOrder, calculatePath(finishNode)];
+    }
+    updateUnvisitedNeighbours(closestNode, grid);
+  }
+}
+
+function getAllNodes(grid = []) {
+  const nodes = [];
+  for (let i = 0; i < grid.size; i++) {
+    for (let j = 0; j < grid[0].size; j++) {
+      nodes.push(grid[i][j]);
+    }
+  }
+  return nodes;
+}
+
+function sortNodes(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+function unvisitedNeighbours(node, grid) {
+  const neighbours = [];
+  const { row, col } = node;
+  if (row > 0) {
+    neighbours.push(grid[row - 1][col]);
+  }
+  if (row < grid.length - 1) {
+    neighbours.push(grid[row + 1][col]);
+  }
+  if (col > 0) {
+    neighbours.push(grid[row][col - 1]);
+  }
+  if (col < grid[0].length - 1) {
+    neighbours.push(grid[row][col + 1]);
+  }
+  return neighbours.filter((neighbour) => !neighbour.isVisited);
+}
+
+function updateUnvisitedNeighbours(node, grid) {
+  const unvisitedNeighbours = unvisitedNeighbours(node, grid);
+  for (const neighbour of unvisitedNeighbours) {
+    neighbour.distance = node.distance + 1;
+    neighbour.previousNode = node;
+  }
+}
+
+function calculatePath(finishNode) {
+  const shortestPathNodes = [];
+  let currentNode = finishNode;
+  while (currentNode != null) {
+    shortestPathNodes.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return shortestPathNodes;
+}
--- react_front/src/algorithms/Graph/Graph.js
+++ react_front/src/algorithms/Graph/Graph.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { Legend } from './Utils/Legend';
 import { c1Dto2D, c2Dto1D } from './Utils/Conversion';
 //import Node from './Node/Node';
+import Dijkstra from './Algorithms/Dijkstra';
 import Maze from './Maze/Maze';
 import './Graph.css';
+
+const ROWS = 37;
+const COLS = 37;
+const START_NODE_STATE = 1;
+const END_NODE_STATE = 2;
+const WALL_NODE_STATE = 3;
+const SPEED = 25;
 
 export default class Graph extends Component {
   render() {
