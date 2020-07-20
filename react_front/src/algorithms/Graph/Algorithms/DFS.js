530faee132abe9e90fe69a7c86e1214cf24e4dc3
Yugant Kachroo <yugant35@gmail.com>
2020-07-20 21:35:32 +0530

    dfs working but still has a bug

--- react_front/src/algorithms/Graph/Algorithms/DFS.js
+++ react_front/src/algorithms/Graph/Algorithms/DFS.js
@@ -1,11 +1,19 @@
 export function DFS(grid = [], startNode, finishNode) {
   const visitedInOrder = [];
-  dfsRecursion(grid, startNode, finishNode, visitedInOrder);
+  visitedInOrder.push(startNode);
+
+  let flag = 0;
+  dfsRecursion(grid, startNode, finishNode, visitedInOrder, flag);
   return [visitedInOrder, calculatePath(finishNode)];
 }
 
-function dfsRecursion(grid = [], node, finishNode, visitedInOrder) {
+function dfsRecursion(grid = [], node, finishNode, visitedInOrder, flag) {
   node.isVisited = true;
+  //visitedInOrder.push(node);
+  if (node === finishNode) {
+    flag = 1;
+    return [visitedInOrder, calculatePath(finishNode)];
+  }
   const x = [0, 0, 1, -1];
   const y = [1, -1, 0, 0];
   for (let i = 0; i < 4; i++) {
@@ -19,19 +27,24 @@ function dfsRecursion(grid = [], node, finishNode, visitedInOrder) {
         newRow < grid.length &&
         newCol < grid[0].length
       )
-    )
+    ) {
       continue;
+    }
     const neighbour = grid[newRow][newCol];
 
     if (neighbour.isWall === true || neighbour.isVisited === true) {
       continue;
     }
     neighbour.previousNode = node;
-    visitedInOrder.push(node);
+    visitedInOrder.push(neighbour);
     if (neighbour === finishNode) {
+      flag = 1;
       return [visitedInOrder, calculatePath(finishNode)];
     }
-    return dfsRecursion(grid, neighbour, finishNode, visitedInOrder);
+    if (flag === 1) {
+      return dfsRecursion(grid, neighbour, finishNode, visitedInOrder);
+    }
+    dfsRecursion(grid, neighbour, finishNode, visitedInOrder);
   }
 }
 
