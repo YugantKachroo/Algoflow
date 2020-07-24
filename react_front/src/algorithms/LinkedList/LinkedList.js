c55f06afa7fe4d6f69dd0b75db6e6d6f6486d930
Yugant Kachroo <yugant35@gmail.com>
2020-07-24 07:43:47 +0530

    node addition logic added

--- react_front/src/algorithms/LinkedList/LinkedList.js
+++ react_front/src/algorithms/LinkedList/LinkedList.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import Bar from '../../components/Bar';
 import './LinkedList.css';
 import Pointer from './pointer.png';
+import './File.css';
 
-let animations = {
-  nodeAnimationTimeout: 1000,
-  pointerAnimationTimeout: 800,
-  deleteTimeout: 1000,
-};
+let nodeAnimationTimeout: 1000;
+let pointerAnimationTimeout: 800;
+let deleteTimeout: 1000;
+let errorCircle = '<i class="fas fa-exclamation-circle"></i> ';
 
 export default class LinkedList extends Component {
   constructor(props) {
@@ -17,19 +17,146 @@ export default class LinkedList extends Component {
     this.ClickAdd = this.ClickAdd.bind(this);
   }
 
-  ClickAdd() {
-    //let userInput = this.getUserInput(this.parentNode);
+  componentDidMount() {
+    this.add(0, 2);
+  }
+
+  ClickAdd(event) {
+    event.preventDefault();
     let nodes = [];
     nodes = document.getElementsByClassName('node');
+    console.log(nodes.length);
+    this.add(nodes.length, this.state.addValue);
     console.log(this.state.addValue);
+  }
 
-    //this.add(nodes.length, userInput.data);
+  checkInputErrors(input, type, endsAtLastNode = false) {
+    let inputError = false;
+    let nodes = document.getElementsByClassName('node');
+    let end = endsAtLastNode ? nodes.length - 1 : nodes.length;
+    let error = document.getElementById('error');
+
+    if (isNaN(input)) {
+      error.innerHTML = errorCircle + type + ' must be a number';
+      inputError = true;
+    } else if (type === 'Index' && (input > end || input < 0)) {
+      error.innerHTML = errorCircle + 'Index Out Of Bounds';
+      inputError = true;
+    }
+
+    if (inputError) error.firstChild.style.animation = 'highlightNode .8s ease';
+    else error.innerHTML = null;
+
+    return inputError;
   }
 
   addHandleChange(event) {
     const temp = event.target.value;
     this.setState({ addValue: temp });
-    // console.log(temp);
+    // console.log(this.state.addValue);
+  }
+
+  async add(i, data) {
+    if (
+      this.checkInputErrors(i, 'Index') ||
+      this.checkInputErrors(data, 'Data')
+    )
+      return;
+
+    let node = document.createElement('div');
+    node.classList.add('node');
+    let number = document.createElement('p');
+    number.classList.add('number');
+    let text = document.createTextNode(data);
+    //console.log(data);
+    number.appendChild(text);
+    node.appendChild(number);
+
+    let pointer = document.createElement('div');
+    pointer.classList.add('pointer');
+    pointer.style.opacity = '0';
+
+    let img = document.createElement('img');
+    img.src = Pointer;
+
+    pointer.appendChild(img);
+    let nodes = document.getElementsByClassName('node');
+    let list = document.getElementById('list');
+
+    if (i === nodes.length) {
+      await this.animateNodes(0, i - 1);
+      list.appendChild(node);
+      list.appendChild(pointer);
+    } else {
+      await this.animateNodes(0, i - 1);
+      await this.animateNodesBeforeInsert(i, nodes.length);
+      list.insertBefore(pointer, nodes[i]);
+      list.insertBefore(node, pointer);
+    }
+
+    node.style.animation =
+      'grow ' + nodeAnimationTimeout / 1000 + 's ' + 'ease';
+
+    setTimeout(() => {
+      pointer.style.opacity = 1;
+      pointer.style.animation =
+        'slide ' + pointerAnimationTimeout / 1000 + 's ' + 'ease';
+    }, nodeAnimationTimeout);
+  }
+
+  async animateNodes(start, end) {
+    for (let i = start; i <= end; i++) {
+      await this.animateNode(i);
+      await this.animatePointer(i);
+    }
+  }
+
+  animateNode(i) {
+    return new Promise((resolve) => {
+      let nodes = document.getElementsByClassName('node');
+      nodes[i].style.animation =
+        'highlightNode ' + nodeAnimationTimeout / 1000 + 's ' + 'ease';
+      setTimeout(() => {
+        nodes[i].style.animation = null;
+        resolve();
+      }, nodeAnimationTimeout);
+    });
+  }
+
+  animatePointer(i) {
+    return new Promise((resolve) => {
+      let pointers = document.getElementsByClassName('pointer');
+      pointers[i].style.animation =
+        'highlightPointer ' + pointerAnimationTimeout / 1000 + 's ' + 'ease';
+      setTimeout(() => {
+        pointers[i].style.animation = null;
+        resolve();
+      }, pointerAnimationTimeout);
+    });
+  }
+
+  animateNodesBeforeInsert(from, to) {
+    return new Promise((resolve) => {
+      let pointers = document.getElementsByClassName('pointer');
+      let nodes = document.getElementsByClassName('node');
+      for (let i = from; i < to; i++) {
+        console.log('length3', nodes.length);
+        console.log(1);
+
+        nodes[i].style.animation =
+          'moveRightNode ' + pointerAnimationTimeout / 1000 + 's ' + 'ease';
+
+        pointers[i].style.animation =
+          'moveRightNode ' + pointerAnimationTimeout / 1000 + 's ' + 'ease';
+
+        setTimeout(() => {
+          nodes[i].style.animation = null;
+          pointers[i].style.animation = null;
+        }, pointerAnimationTimeout);
+      }
+
+      setTimeout(() => resolve(), pointerAnimationTimeout);
+    });
   }
 
   render() {
